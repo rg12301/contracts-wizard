c32e49eadc483b80da6390536438959da6debda9
Eric Lau <ericglau@outlook.com>
2022-06-10 11:30:44 -0400

    Add getInitialSupply and toUint256 functions

--- packages/core-cairo/src/api.ts
+++ packages/core-cairo/src/api.ts
@@ -1,6 +1,7 @@
 import type { CommonOptions } from './common-options';
-import { printERC20, defaults as erc20defaults, ERC20Options } from './erc20';
+import { printERC20, defaults as erc20defaults, ERC20Options, getInitialSupply } from './erc20';
 import { printERC721, defaults as erc721defaults, ERC721Options } from './erc721';
+import { toUint256 } from './utils/uint256';
 
 export interface WizardContractAPI {
   /**
@@ -16,16 +17,26 @@ export interface WizardContractAPI {
 
 export interface ERC20 extends WizardContractAPI {
   defaults: Required<ERC20Options>;
+
+  /**
+   * Calculates the initial supply that would be used in an ERC20 contract based on a given premint amount and number of decimals.
+   */
+  getInitialSupply: (premint: string, decimals: number) => string;
 }
+
 export interface ERC721 extends WizardContractAPI {
   defaults: Required<ERC721Options>;
 }
 
 export const erc20: ERC20 = {
   print: printERC20,
-  defaults: erc20defaults
+  defaults: erc20defaults,
+  getInitialSupply
 }
 export const erc721 = {
   print: printERC721,
   defaults: erc721defaults
+}
+export const utils = {
+  toUint256
 }
